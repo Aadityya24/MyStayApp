cc82be6e0e7f4801e0e3acc312c571c227168dcd
Aadityya24 <aadityyaggholasgaon@gmail.com>
2026-03-05 09:58:50 +0530

    UC1: Add welcome message module

--- src/MyStayApp.java
+++ src/MyStayApp.java
@@ -1,2 +1,11 @@
 public class MyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to MyStay Hotel Booking");
+        System.out.println("Version 1.0");
+        System.out.println("=================================");
+
+    }
 }
